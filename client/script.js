7ed3267a71207185279d239e7aecf8ed1371494b
Sumit Patel <sumitz1207@gmail.com>
2023-04-03 23:24:08 -0400

    begin handlesubmit and event listeners

--- client/script.js
+++ client/script.js
@@ -57,3 +57,21 @@ function chatStripe(isAi, value, uniqueId) {
   `
   )
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  form.reset()
+
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv)
